da3304c8eff2ce2c07edda2051cdeb4f74574516
Greene <trentgreene@trishs-imac.local>
2018-05-15 18:57:22 -0500

    Initial Commit

---
+++ kitsune/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kitsune
+//
+//  Created by Trent Greene on 5/15/18.
+//  Copyright © 2018 greene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
