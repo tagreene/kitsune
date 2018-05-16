248b6d2cc5ad20da18ac3a2a920758fdc15529b8
greene <greene@greenes-MacBook-Pro.local>
2018-05-15 19:13:40 -0500

    First commit

---
+++ Pods/Target Support Files/Pods-kitsuneTests/Pods-kitsuneTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kitsuneTests : NSObject
+@end
+@implementation PodsDummy_Pods_kitsuneTests
+@end
---
+++ Pods/Target Support Files/Pods-kitsuneUITests/Pods-kitsuneUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kitsuneUITests : NSObject
+@end
+@implementation PodsDummy_Pods_kitsuneUITests
+@end
