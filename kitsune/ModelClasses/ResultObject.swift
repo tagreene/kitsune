3cce3dc900328a273f13089a0a8846ad33dff263
greene <greene@greenes-MacBook-Pro.local>
2018-05-18 16:05:19 -0500

    Add convenience init for ResultObject

--- kitsune/ModelClasses/ResultObject.swift
+++ kitsune/ModelClasses/ResultObject.swift
@@ -10,5 +10,33 @@ import Foundation
 import CoreData
 
 public class ResultObject: NSManagedObject {
+    convenience init(withResult result: Result, entity: NSEntityDescription, context: NSManagedObjectContext) {
+        self.init(entity: entity, insertInto: context)
+        
+        self.price = result.price
+        self.id = result.id
+        self.name = result.name
+        self.url = result.url
+        self.imageURL = result.image_url
+        self.country = result.location?.country
+        self.state = result.location?.state
+        self.city = result.location?.city
+        self.zipCode = result.location?.zip_code
+        self.address1 = result.location?.address1
+        self.address2 = result.location?.address2
+        self.address3 = result.location?.address3
+        
+        self.lastSeen = Date()
+        self.favorited = false
+        self.rejected = false
+        
+        self.categories = [String: String]()
+        if let categories = result.categories {
+            for category in categories {
+                // TODO: Make sure the bangs below are safe, or unwrap them correctly
+                self.categories?[category.title!] = category.alias!
+            }
+        }
+    }
     
 }
